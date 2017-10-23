cbdeac4479354ee1b72127202d83edab0f5d7ccd
Douglas Fils <dfils@oceanleadership.org>
2017-10-23 16:29:48 -0500

    Few simple edits to fix the webapps docker file

--- webapp/main.go
+++ webapp/main.go
@@ -28,6 +28,11 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// root
+	parking := mux.NewRouter()
+	parking.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./static"))))
+	http.Handle("/", &MyServer{parking})
+
 	// Recall /id is going to be our dx..   all items that come in with that will be looked up and 303'd
 	// Example URL:  http://opencoredata.org/id/dataset/c2d80e2a-cc30-430c-b0bd-cee9092688e3
 	dxroute := mux.NewRouter()
@@ -35,18 +40,15 @@ func main() {
 	dxroute.HandleFunc("/id/graph/{id}/provenance", dx.ProvRedirection)   // PROV: prov redirection
 	dxroute.HandleFunc("/id/graph/{id}/pingback", dx.PingbackRedirection) // PROV: pingback for this resource  (would prefer a master /prov or server)
 	dxroute.HandleFunc("/id/dataset/{UUID}", dx.Redirection)
-	dxroute.HandleFunc("/id/expedition/{LEG}/{SITE}/{HOLE}", dx.Redirection)
-	dxroute.HandleFunc("/id/expedition/{LEG}/{SITE}", dx.Redirection)
-	dxroute.HandleFunc("/id/expedition/{LEG}", dx.Redirection)
 	dxroute.HandleFunc(`/id/resource/{resourcepath:[a-zA-Z0-9=\-\/]+}`, dx.Redirection)
 	http.Handle("/id/", dxroute)
 
 	// Some early Prov Pingback work here...   Deal with void...  (show void..  allow .rdf file downloads)
 	dataset := mux.NewRouter()
-	dataset.HandleFunc("/rdf/graph/{id}", handlers.RenderWithProvHeader)      // PROV: test cast with Void..  would need to generalize
-	dataset.HandleFunc("/rdf/graph/{id}/provenance", handlers.RenderWithProv) // PROV: test cast with Void..  would need to generalize
-	dataset.HandleFunc("/rdf/graph/{id}/pingback", handlers.ProvPingback)     // PROV: pingback for this resource  (would prefer a master /prov or server)
-	http.Handle("/rdf/", dataset)
+	dataset.HandleFunc("/doc/dataset/{id}", handlers.RenderWithProvHeader)      // PROV: test cast with Void..  would need to generalize
+	dataset.HandleFunc("/doc/dataset/{id}/provenance", handlers.RenderWithProv) // PROV: test cast with Void..  would need to generalize
+	dataset.HandleFunc("/doc/dataset/{id}/pingback", handlers.ProvPingback)     // PROV: pingback for this resource  (would prefer a master /prov or server)
+	http.Handle("/doc/", dataset)
 
 	// Start the server...
 	log.Printf("About to listen on 9900. Go to http://127.0.0.1:9900/")
