0faf2a21ed669f917619a8f56fe78af1201f973f
Douglas Fils <dfils@oceanleadership.org>
2017-12-01 12:16:21 -0600

    Some service updates

--- webapp/services/services.go
+++ webapp/services/services.go
@@ -23,19 +23,29 @@ func ProvEventInfo(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(entry))
 }
 
+func QueryServiceOpenSearch(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Whoa there....  you have reached the edge of our ever expanding universe"))
+}
+
+func QueryServiceSwagger(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Whoa there....  you have reached the edge of our ever expanding universe"))
+}
+
+func QueryServiceSchema(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Whoa there....  you have reached the edge of our ever expanding universe"))
+}
+
 // DocGraph returns prov for a given document ID
 // This is RDF prov..  not a URI list..   so..  we need policy on our URI-list holdings
 // also, use the pattern:
 // http://www.example.com/provenance/service?target={+uri}{&steps}
 // as noted in the docs.  Optional steps can be, for us, scope [curated, community]
 func DocGraph(w http.ResponseWriter, r *http.Request) {
-
 	w.Write([]byte("Whoa there....  you have reached the edge of our ever expanding universe"))
 }
 
 // DocReport should return all we know about events on the document.  POSTings,
 // local graph, URI lists, community graph obtained, etc...
 func DocReport(w http.ResponseWriter, r *http.Request) {
-
 	w.Write([]byte("Whoa there....  you have reached the edge of our ever expanding universe"))
 }
