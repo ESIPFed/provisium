1de6983b050e58c0a56313a9af59738bb11fd00b
Tom Narock <tnarock@ndm.edu>
2018-06-01 11:33:20 -0400

    Updated API with changes for USGS use case

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"net/http"
 	"os"
-
 	log "github.com/sirupsen/logrus"
 	"lab.esipfed.org/provisium/pkg/kv"
-	"lab.esipfed.org/provisium/pkg/objectservices"
-	"lab.esipfed.org/provisium/pkg/pingback"
-	"lab.esipfed.org/provisium/pkg/query"
+	"lab.esipfed.org/provisium/pkg/prov"
+	"lab.esipfed.org/provisium/pkg/api"
+	util "lab.esipfed.org/provisium/pkg/util"
 
 	restful "github.com/emicklei/go-restful"
 	swagger "github.com/emicklei/go-restful-swagger12"
+
 )
 
 func init() {
@@ -21,19 +21,20 @@ func init() {
 }
 
 func main() {
-	// Set up our log file for runs...
-	f, err := os.OpenFile("./logs/serviceslog.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
+
+	// Read the provisium config file
+	err := util.ReadConfig()
+	util.WriteToLog("fatal", "main.go, Unable to read provisium congig file: ", err) 
+
+	// Set up our log file 
+	f, err := os.OpenFile(util.Configuration.LogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	util.WriteToLog("fatal", "main.go, Error opening log file: ", err)
 	defer f.Close()
 	log.SetOutput(f)
 
 	// set up the KV store
-	err = kv.InitKV()
-	if err != nil {
-		log.Fatal(err) // fatal..  no bucket..  no reason
-	}
+	err = kv.InitKV(util.Configuration.KvStoreAPI)
+	util.WriteToLog("fatal", "main.go, Error setting up KV store: ", err)
 
 	wsContainer := restful.NewContainer()
 
@@ -49,19 +50,18 @@ func main() {
 	wsContainer.Filter(wsContainer.OPTIONSFilter)
 
 	// Add the services
-	wsContainer.Add(pingback.New())       // text search services
-	wsContainer.Add(query.New())          // text search services
-	wsContainer.Add(objectservices.New()) // text search services
+	wsContainer.Add(prov.New())          
+	wsContainer.Add(api.New())
 
 	// Swagger
-	config := swagger.Config{
+	swaggerConfig := swagger.Config{
 		WebServices:    wsContainer.RegisteredWebServices(), // you control what services are visible
 		ApiPath:        "/apidocs.json",
-		WebServicesUrl: "http://provisium.io"} // localhost:6789
-	swagger.RegisterSwaggerService(config, wsContainer)
+		WebServicesUrl: "http://provisium.io"} // localhost
+	swagger.RegisterSwaggerService(swaggerConfig, wsContainer)
 
 	// Start up
-	log.Printf("Services on localhost:6789")
-	server := &http.Server{Addr: ":6789", Handler: wsContainer}
+	log.Printf("Services on localhost:" + util.Configuration.Port)
+	server := &http.Server{Addr: ":" + util.Configuration.Port, Handler: wsContainer}
 	server.ListenAndServe()
 }
--- pkg/kv/kv.go
+++ pkg/kv/kv.go
@@ -3,53 +3,18 @@ package kv
 import (
 	"fmt"
 	"log"
-	"strings"
-	"time"
 
 	"github.com/boltdb/bolt"
-	"github.com/google/uuid"
 )
 
-func StorePB(ID string, buf []byte) error {
+// Write api key and name
+func WriteApiKeyAndName (kvStore, apiKey, name string) error {
 
-	return nil
-}
+	db := getKVStoreRW(kvStore)
 
-// NewProvEvent must address a range of actions.  On a new event
-// we need to record the CLF of the event, the prov graph fragment
-// and associate the new prov event ID with the document ID
-func NewProvEvent(docID, provFrag, remoteAddress, contentType string) error {
-
-	provID := uuid.New().String()
-	fmt.Printf("For doc %s I am recording a new event %s \n", docID, provID)
-
-	// Need to try and make this transactional at some point...
-	// Out of scope initially for the project...
-	// would likely have to use some roll back on a not nil event
-
-	db := getKVStoreRW()
-
-	// TODO..  connect these three updates into a single transaction wrapper
-	// Log the event
 	db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("LogBucket"))
-		t := time.Now()
-		logEvent := fmt.Sprintf("%s, %s, %s", remoteAddress, t.Format(time.RFC3339), contentType)
-		err := b.Put([]byte(provID), []byte(logEvent))
-		return err
-	})
-
-	// Record the Prov
-	db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("ProvBucket"))
-		err := b.Put([]byte(provID), []byte(provFrag))
-		return err
-	})
-
-	// Associate DocID and ProvID
-	db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("IDLinkBucket"))
-		err := b.Put([]byte(provID), []byte(docID))
+		b := tx.Bucket([]byte("ApiKeys"))
+		err := b.Put([]byte(apiKey), []byte(name))
 		return err
 	})
 
@@ -58,230 +23,93 @@ func NewProvEvent(docID, provFrag, remoteAddress, contentType string) error {
 	return nil
 }
 
-// TODO..  get this the prov vetted by provider
-// GetProvCuratedGraph
+// Delete url and api key from the key store
+func DeleteUrlAndKey (kvStore, url string) error {
 
-// TODO..  get the graph that is the roll up of all
-// prov sent in.  Which means I need to follow up on the
-// URI-Lists and build a graph from them.
-// GetProvCommunityGraph
+	db := getKVStoreRW(kvStore)
 
-//GetProvDetails Return the entry for a specific prov record
-func GetProvDetails(provID string) (string, string, error) {
-	fmt.Printf("Request content provID %s \n", provID)
-	db := getKVStoreRO()
-
-	var provEntry string
-	var contentType string
-	err := db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("ProvBucket"))
-		b2 := tx.Bucket([]byte("LogBucket"))
-
-		v := b.Get([]byte(provID))
-		provEntry = string(v)
-
-		v2 := b2.Get([]byte(provID))
-		logLine := strings.Split(string(v2), ",") // 3rd entry is content-type, see NewProvEvent write to this bucket
-		if len(logLine) == 3 {
-			contentType = logLine[2]
-		} else {
-			contentType = "text/plain" // ??
-		}
-
-		log.Println(logLine)
-
-		return nil
-	})
-
-	if err != nil {
-		log.Println("Error reading from Buckets")
-	}
-
-	err = db.Close()
-	if err != nil {
-		log.Println("Error closing database index.db")
-		log.Println(err)
-	}
-
-	return provEntry, contentType, nil
-}
-
-// GetProvLog gets all the logged events for a given docID
-func GetProvLog(docID string) (map[string]string, error) {
-	db := getKVStoreRO()
-
-	eventmap := make(map[string]string)
-
-	// Logic needed
-	// 1) loop over IDLinkBucket to find all provID that match a value of docID
-	// 2) for each provID, pull event (value) from LogBucket
-	err := db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("IDLinkBucket"))
-		b2 := tx.Bucket([]byte("LogBucket"))
-		c := b.Cursor()
-		for k, v := c.First(); k != nil; k, v = c.Next() {
-			if strings.Contains(string(v), docID) {
-				v2 := b2.Get(k)
-				eventmap[string(k)] = string(v2)
-			}
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Println("Error reading file info from the KV store index.db")
-		log.Println(err)
-	}
-
-	err = db.Close()
-	if err != nil {
-		log.Println("Error closing database index.db")
-		log.Println(err)
-	}
-
-	return eventmap, err
-}
-
-// GetDocIDs get all the files in our holding
-func GetDocIDs() []string {
-	db := getKVStoreRO()
-
-	var IDs []string
-	err := db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("MetaDataBucket"))
-		c := b.Cursor()
-		for k, _ := c.First(); k != nil; k, _ = c.Next() {
-			// log.Printf("key=%s, value=%s\n", k, v)
-			IDs = append(IDs, string(k))
-		}
-		return nil
-	})
-
-	// TODO..  add in doing this for external resources too
-
-	if err != nil {
-		log.Println("Error reading file info from the KV store index.db")
-		log.Println(err)
-	}
-
-	err = db.Close()
-	if err != nil {
-		log.Println("Error closing database index.db")
-		log.Println(err)
-	}
-
-	return IDs
-}
-
-// GetResMetaData will get the metadata for a dataset
-func GetResMetaData(docID string) (string, error) {
-	fmt.Printf("I will get the metadata for docID %s \n", docID)
-	db := getKVStoreRO()
-
-	var jsonld string
-	err := db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("MetaDataBucket"))
-		v := b.Get([]byte(docID))
-		jsonld = string(v)
-		return nil
-	})
-
-	if err != nil {
-		log.Println("Error reading file info from the KV store index.db")
-	}
-
-	err = db.Close()
-	if err != nil {
-		log.Println("Error closing database index.db")
-		log.Println(err)
-	}
-
-	return jsonld, err
-}
-
-// GetResData will get the metadata for a dataset
-func GetResData(docID string) (string, error) {
-	fmt.Printf("I will get the data for docID %s \n", docID)
-	db := getKVStoreRO()
-
-	var datafile string
-	err := db.View(func(tx *bolt.Tx) error {
+	db.Update(func(tx * bolt.Tx) error {
 		b := tx.Bucket([]byte("FileBucket"))
-		v := b.Get([]byte(docID))
-		datafile = string(v)
-		return nil
-	})
-
-	if err != nil {
-		log.Println("Error reading file info from the KV store index.db")
-	}
-
-	err = db.Close()
-	if err != nil {
-		log.Println("Error closing database index.db")
-		log.Println(err)
-	}
-
-	return datafile, err
-}
-
-// SetResDataByRef take a URI reference and enters that as a
-// resources in the system.
-func SetResDataByRef(ref string) (string, error) {
-	fmt.Printf("I will set the data for reference %s \n", ref)
-	db := getKVStoreRW()
-
-	docID := uuid.New().String()
-
-	err := db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("RefBucket"))
-		err := b.Put([]byte(docID), []byte(ref))
+		err := b.Delete([]byte(url))
 		return err
 	})
 
-	if err != nil {
-		log.Println("Error writing reference info from the KV store index.db Filebucket")
-	}
+	db.Close()
 
-	err = db.Close()
+	return nil
+
+}
+
+// Write url and api key to the key store
+func WriteUrlAndKey (kvStore, url, apiKey string) error {
+
+	db := getKVStoreRW(kvStore)
+
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("FileBucket"))
+		err := b.Put([]byte(url), []byte(apiKey))
+		return err
+	})
+
+	db.Close()
+
+	return nil
+}
+
+// ValidateApiKey will validate an API Key (true/false) 
+func ValidateApiKey(apiKey string, kvStore string) bool {
+
+	db := getKVStoreRO(kvStore)
+
+	isValid := false
+	name := ""
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("ApiKeys"))
+		v := b.Get([]byte(apiKey))
+		name = string(v)
+		return nil
+	})
+
 	if err != nil {
-		log.Println("Error closing database index.db")
+		log.Println("Error viewing ApiKeys key value store")
 		log.Println(err)
 	}
 
-	return docID, err
+	if name != "" { isValid = true } else { isValid = false } 
+
+	err = db.Close()
+	if err != nil {
+		log.Println("Error closing key value database")
+		log.Println(err)
+	}
+
+	return isValid
 }
 
-// Util functions for KV operations below here
-
-func getKVStoreRW() *bolt.DB {
-	db, err := bolt.Open("./dataVolume/kvStores/index.db", 0666, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+func getKVStoreRW(kvStore string) *bolt.DB {
+	db, err := bolt.Open(kvStore, 0666, nil)
+	if err != nil { log.Fatal(err) }
 	// defer db.Close()
 
 	return db
 }
 
-func getKVStoreRO() *bolt.DB {
-	db, err := bolt.Open("./dataVolume/kvStores/index.db", 0666, &bolt.Options{ReadOnly: true})
-	if err != nil {
-		log.Fatal(err)
-	}
+func getKVStoreRO(kvStore string) *bolt.DB {
+	db, err := bolt.Open(kvStore, 0666, &bolt.Options{ReadOnly: true})
+	if err != nil { log.Fatal(err) }
 	// defer db.Close()
 
 	return db
 }
 
 // Init the KV store in case we are starting empty and need some buckets made
-// Call from the main program at run time...
-func InitKV() error {
+// Called from the main program at run time...
+func InitKV(kvStore string) error {
 
-	db := getKVStoreRW()
+	db := getKVStoreRW(kvStore)
 
 	err := db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte("RefBucket"))
+		_, err := tx.CreateBucketIfNotExists([]byte("ApiKeys"))
 		if err != nil {
 			return fmt.Errorf("create bucket: %s", err)
 		}
@@ -296,37 +124,6 @@ func InitKV() error {
 		return nil
 	})
 
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte("MetaDataBucket"))
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
-		}
-		return nil
-	})
-
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte("IDLinkBucket"))
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
-		}
-		return nil
-	})
-
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte("ProvBucket"))
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
-		}
-		return nil
-	})
-
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte("LogBucket"))
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
-		}
-		return nil
-	})
 
 	db.Close()
 
